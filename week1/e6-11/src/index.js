2688f4f8ddc60f8090538774c95e249765080def
Joni Salmi <joni@josalmi.fi>
2018-01-21 15:11:42 +0200

    W1E6: Add simple counter

--- week1/e6-11/src/index.js
+++ week1/e6-11/src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
-    <div>Unicafe</div>
+const Counter = ({ onIncrement }) => (
+    <button type="button" onClick={onIncrement}>+</button>
 )
+
+class App extends Component {
+
+    state = {
+        counter: 0
+    }
+
+    handleIncrement = () => (
+        this.setState((prevState) => ({ counter: prevState.counter + 1 }))
+    )
+
+    render() {
+        return (
+            <div>
+                <Counter onIncrement={this.handleIncrement} />
+                {this.state.counter}
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
