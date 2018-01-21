a63b9076cde2ea489da57e7efa81f41d1a3eb949
Joni Salmi <joni@josalmi.fi>
2018-01-21 14:03:22 +0200

    W1: Updated readme to contain completed exercises

---
+++ week1/README.md
@@ -0,0 +1,9 @@
+# Week 1
+
+## Part 1 ([solution](e1-5))
+
+- [ ] 1.1 jako komponenteiksi
+- [ ] 1.2 lisää komponentteja
+- [ ] 1.3 tieto olioissa
+- [ ] 1.4 oliot taulukkoon
+- [ ] 1.5
---
+++ week1/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const kurssi = 'Half Stack -sovelluskehitys'
+  const osa1 = 'Reactin perusteet'
+  const tehtavia1 = 10
+  const osa2 = 'Tiedonvälitys propseilla'
+  const tehtavia2 = 7
+  const osa3 = 'Komponenttien tila'
+  const tehtavia3 = 14
+
+  return (
+    <div>
+      <h1>{kurssi}</h1>
+      <p>{osa1} {tehtavia1}</p>
+      <p>{osa2} {tehtavia2}</p>
+      <p>{osa3} {tehtavia3}</p>
+      <p>yhteensä {tehtavia1 + tehtavia2 + tehtavia3} tehtävää</p>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
