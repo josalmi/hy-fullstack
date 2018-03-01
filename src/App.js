de23e78652c13490b3a9e9b5d0a6663b27b5837d
Joni Salmi <joni@josalmi.fi>
2018-03-02 01:21:34 +0200

    Squashed 'week5/redux-anecdotes/' content from commit 1d61655
    git-subtree-dir: week5/redux-anecdotes
    git-subtree-split: 1d616557f8459a67baf4e0cea2f6091175f95355

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  render() {
+    const anecdotes = this.props.store.getState()
+    return (
+      <div>
+        <h2>Anecdotes</h2>
+        {anecdotes.map(anecdote=>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content} 
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button>vote</button>
+            </div>
+          </div>
+        )}
+        <h2>create new</h2>
+        <form>
+          <div><input /></div>
+          <button>create</button> 
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
